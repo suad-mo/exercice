01033f7592b90f4f4df5a26d2a89b330a1eff045
Suad Krvavac <suad.krvavac@gmail.com>
2022-11-15 15:26:38 +0100

    Silver app bar 02...

--- lib/main.dart
+++ lib/main.dart
@@ -1,134 +1,137 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: NewsScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class NewsScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  final List<String> _tabs = <String>[
+    "Featured",
+    "Popular",
+    "Latest",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    const title = 'GeeksforGeeks';
-
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              snap: false,
-              pinned: true,
-              floating: false,
-              flexibleSpace: FlexibleSpaceBar(
-                  // centerTitle: true,
-                  // // titlePadding: EdgeInsets.only(bottom: 0),
-                  // title: const Text(title,
-                  //     style: TextStyle(
-                  //       color: Colors.white,
-                  //       fontSize: 16.0,
-                  //     ) //TextStyle
-                  //     ), //Text
-                  background: Stack(
-                fit: StackFit.loose,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    child: Image.network(
-                      "https://i.ibb.co/QpWGK5j/Geeksfor-Geeks.png",
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                  Positioned(
-                    top: 20,
-                    right: 20,
-                    child: Row(
-                      children: [
-                        Container(
-                          // margin: EdgeInsets.all(5),
-                          // decoration: BoxDecoration(
-                          //     border: Border.all(width: 2),
-                          //     color: Colors.green),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            border: Border.all(color: Colors.red),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 5),
-                          // color: Colors.redAccent,
-                          child: Text(1.10.toStringAsFixed(1)),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 20,
-                    left: 20,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        border: Border.all(color: Colors.red),
-                        color: Colors.black26,
+    return Material(
+      child: Scaffold(
+        body: DefaultTabController(
+          length: _tabs.length,
+          child: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverOverlapAbsorber(
+                  handle:
+                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                  sliver: SliverSafeArea(
+                    left: false,
+                    top: true,
+                    bottom: Platform.isIOS ? false : true,
+                    sliver: SliverAppBar(
+                      title: Text('Tab Demo'),
+                      floating: true,
+                      pinned: false,
+                      snap: true,
+                      expandedHeight: 300,
+                      collapsedHeight: 100,
+                      flexibleSpace: Container(
+                        height: 200,
+                        color: Colors.green,
+                        child: const Center(
+                            child: Icon(
+                          Icons.chair,
+                          size: 42,
+                        )),
                       ),
-                      // backgroundBlendMode: BlendMode.color),
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      // color: Colors.redAccent,
-                      child: Text(1.10.toStringAsFixed(1)),
+                      forceElevated: innerBoxIsScrolled,
+                      bottom: TabBar(
+                        tabs: _tabs
+                            .map((String name) => Tab(text: name))
+                            .toList(),
+                      ),
                     ),
                   ),
-                ],
-              ) //Images.network
-                  ), //FlexibleSpaceBar
-              expandedHeight: 280,
-              backgroundColor: Colors.greenAccent[400],
-              leading: IconButton(
-                icon: const Icon(Icons.menu),
-                tooltip: 'Menu',
-                onPressed: () {},
-              ),
-              bottom: const TabBar(
-                // padding: EdgeInsets.only(bottom: 30),
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.cloud_outlined)),
-                  Tab(icon: Icon(Icons.beach_access_sharp)),
-                  Tab(icon: Icon(Icons.brightness_5_sharp)),
-                ],
-              ),
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.comment),
-                  tooltip: 'Comment Icon',
-                  onPressed: () {},
-                ), //IconButton
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  tooltip: 'Setting Icon',
-                  onPressed: () {},
-                ), //IconButton
-              ], //<Widget>[]
-            ), //SliverAppBar
-
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) => ListTile(
-                  tileColor: (index % 2 == 0) ? Colors.white : Colors.green[50],
-                  title: Center(
-                    child: Text('$index',
-                        style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 50,
-                            color: Colors.greenAccent[400]) //TextStyle
-                        ), //Text
-                  ), //Center
-                ), //ListTile
-                childCount: 51,
-              ), //SliverChildBuildDelegate
-            ) //SliverList
-          ], //<Widget>[]
-        ) //CustonScrollView
+                ),
+              ];
+            },
+            body: TabBarView(
+              children: _tabs.map((String name) {
+                return SafeArea(
+                  top: false,
+                  bottom: false,
+                  child: Builder(
+                    builder: (BuildContext context) {
+                      return NotificationListener<ScrollNotification>(
+                        onNotification: (scrollNotification) {
+                          return true;
+                        },
+                        child: CustomScrollView(
+                          key: PageStorageKey<String>(name),
+                          slivers: <Widget>[
+                            SliverOverlapInjector(
+                              handle: NestedScrollView
+                                  .sliverOverlapAbsorberHandleFor(context),
+                            ),
+                            SliverPadding(
+                              padding: const EdgeInsets.all(8.0),
+                              sliver: SliverList(
+                                delegate: SliverChildBuilderDelegate(
+                                  (BuildContext context, int index) {
+                                    return Column(
+                                      children: <Widget>[
+                                        Container(
+                                          height: 150,
+                                          width: double.infinity,
+                                          color: Colors.blueGrey,
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text('$name $index')
+                                            ],
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 8,
+                                        )
+                                      ],
+                                    );
+                                  },
+                                  childCount: 30,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }).toList(),
             ),
-      ), //Scaffold
-      debugShowCheckedModeBanner: false,
-      // Remove debug banner for proper
-      // view of setting icon
-    ); //MaterialApp
+          ),
+        ),
+      ),
+    );
   }
 }
