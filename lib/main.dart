d984ca850a2920de38233ce16953467be6238c5e
Suad Krvavac <suad.krvavac@gmail.com>
2022-11-17 14:15:29 +0100

    NestedScrollView whose header contains a floating SliverAppBar...

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,135 +5,48 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: const NewsScreen(),
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
     );
   }
 }
 
-class NewsScreen extends StatefulWidget {
-  const NewsScreen({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _NewsScreenState();
-}
-
-class _NewsScreenState extends State<NewsScreen> {
-  final List<String> _tabs = <String>[
-    "Featured",
-    "Popular",
-    "Latest",
-  ];
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        body: DefaultTabController(
-          length: _tabs.length,
-          child: NestedScrollView(
+    return Scaffold(
+        body: NestedScrollView(
+            // Setting floatHeaderSlivers to true is required in order to float
+            // the outer slivers over the inner scrollable.
+            floatHeaderSlivers: true,
             headerSliverBuilder:
                 (BuildContext context, bool innerBoxIsScrolled) {
               return <Widget>[
-                SliverOverlapAbsorber(
-                  handle:
-                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                  sliver: SliverSafeArea(
-                    left: false,
-                    top: true,
-                    bottom: Platform.isIOS ? false : true,
-                    sliver: SliverAppBar(
-                      title: const Text('Tab Demo'),
-                      floating: true,
-                      pinned: false,
-                      snap: true,
-                      expandedHeight: 300,
-                      collapsedHeight: 100,
-                      flexibleSpace: Container(
-                        height: 200,
-                        color: Colors.green,
-                        child: const Center(
-                            child: Icon(
-                          Icons.chair,
-                          size: 42,
-                        )),
-                      ),
-                      forceElevated: innerBoxIsScrolled,
-                      bottom: TabBar(
-                        tabs: _tabs
-                            .map((String name) => Tab(text: name))
-                            .toList(),
-                      ),
-                    ),
-                  ),
+                SliverAppBar(
+                  title: const Text('Floating Nested SliverAppBar'),
+                  floating: true,
+                  //snap: false,
+                  pinned: true,
+                  expandedHeight: 200.0,
+                  forceElevated: !innerBoxIsScrolled,
                 ),
               ];
             },
-            body: TabBarView(
-              children: _tabs.map((String name) {
-                return SafeArea(
-                  top: false,
-                  bottom: false,
-                  child: Builder(
-                    builder: (BuildContext context) {
-                      return NotificationListener<ScrollNotification>(
-                        onNotification: (scrollNotification) {
-                          return true;
-                        },
-                        child: CustomScrollView(
-                          key: PageStorageKey<String>(name),
-                          slivers: <Widget>[
-                            SliverOverlapInjector(
-                              handle: NestedScrollView
-                                  .sliverOverlapAbsorberHandleFor(context),
-                            ),
-                            SliverPadding(
-                              padding: const EdgeInsets.all(8.0),
-                              sliver: SliverList(
-                                delegate: SliverChildBuilderDelegate(
-                                  (BuildContext context, int index) {
-                                    return Column(
-                                      children: <Widget>[
-                                        Container(
-                                          height: 150,
-                                          width: double.infinity,
-                                          color: Colors.blueGrey,
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: <Widget>[
-                                              Text('$name $index')
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 8,
-                                        )
-                                      ],
-                                    );
-                                  },
-                                  childCount: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
-      ),
-    );
+            body: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: 30,
+                itemBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    height: 50,
+                    child: Center(child: Text('Item $index')),
+                  );
+                })));
   }
 }
