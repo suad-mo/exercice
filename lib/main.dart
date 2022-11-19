4f0de32af6b39e195644833b1a484b7406b0dae6
suad-krvavac <suad.krvavac@gmail.com>
2022-11-19 15:33:59 +0100

    Good example...

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,106 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: MyStatelessWidget(),
+      home: MainCollapsingToolbar(),
     );
+  }
+}
+
+class MainCollapsingToolbar extends StatelessWidget {
+  const MainCollapsingToolbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  title: const Text('Colapsing Toolbar'),
+                  automaticallyImplyLeading: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                      // centerTitle: true,
+                      // title: const Text("Collapsing Toolbar",
+                      //     style: TextStyle(
+                      //       color: Colors.white,
+                      //       fontSize: 16.0,
+                      //     )),
+                      background: Image.network(
+                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    fit: BoxFit.cover,
+                  )),
+                ),
+                SliverPersistentHeader(
+                  delegate: _SliverAppBarDelegate(
+                    const TabBar(
+                      labelColor: Colors.blue,
+                      unselectedLabelColor: Colors.grey,
+                      // labelColor: Colors.black87,
+                      // unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        Tab(
+                            child: Text('Tab 1',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16))),
+                        Tab(
+                            child: Text('Tab 2',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16))),
+
+                        // Tab(icon: Icon(Icons.info), text: "Tab 1"),
+                        // Tab(icon: Icon(Icons.lightbulb_outline), text: "Tab 2"),
+                      ],
+                    ),
+                  ),
+                  pinned: true,
+                  floating: false,
+                ),
+              ];
+            },
+            body: const TabBarView(
+              children: [
+                Center(
+                  child: Text("Sample text 1"),
+                ),
+                Center(
+                  child: Text("Sample text 2"),
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
 
